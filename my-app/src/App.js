d2e865bdcfe656f3c27cc6b35cda0274f829373b
nightblade9 <nightbladecodes@gmail.com>
2019-03-04 14:21:59 -0500

    Update and clena up App tests.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,23 +10,6 @@ class App extends Component {
       <div id="container">
         <GameGrid />
       </div>
-
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <p>
-      //       Edit <code>src/App.js</code> and save to reload.
-      //     </p>
-      //     <a
-      //       className="App-link"
-      //       href="https://reactjs.org"
-      //       target="_blank"
-      //       rel="noopener noreferrer"
-      //     >
-      //       Learn React
-      //     </a>
-      //   </header>
-      // </div>
     );
   }
 }
--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import GameGrid from './GameGrid';
 
-it('renders without crashing', () => {
+it('renders without crashing and has a container div with a gamegrid', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+  expect(div.children.length).toBe(1);
+
+  var containerDiv = div.children[0];
+  expect(containerDiv.id).toBe("container")
+  expect(containerDiv.children.length).toBe(1)
+
+  var gameGrid = containerDiv.children[0];
+  expect(gameGrid.id).toBe("grid");
+
   ReactDOM.unmountComponentAtNode(div);
 });
