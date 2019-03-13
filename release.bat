4eeb8c6f6e3b106a948622232e7b5ccdc542e4c0
nightblade9 <nightbladecodes@gmail.com>
2019-03-13 12:54:16 -0400

    Proper push-button release script

--- release.bat
+++ release.bat
@@ -1,4 +1,29 @@
 @echo off
+
+set TEMP_DIR=..\GHPAGES
+set SOURCE_DIR=space-marine-junaid
+
+REM delete and recreate the temp dir
+rd /s /q %TEMP_DIR%
 cd my-app
-npm run build
-echo static site ready to deploy from "build" directory!
+rd /s /q build
+
+REM seed it with .git
+mkdir ..\%TEMP_DIR%
+mkdir ..\%TEMP_DIR%\.git
+xcopy /Y /s ..\.git ..\%TEMP_DIR%\.git
+
+cd ..\%TEMP_DIR%
+git checkout -f gh-pages
+git pull
+
+cd ..\%SOURCE_DIR%\my-app
+cmd /c npm run build
+
+cd ..\%TEMP_DIR%
+echo git add -A .
+echo git commit . -m "Publish latest version"
+echo git push
+
+cd ..\%SOURCE_DIR%
+echo Pushed to gh-pages.
